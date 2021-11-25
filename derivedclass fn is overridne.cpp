d8d220488dc6b465b86271fc22a7769640576283
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-11-25 10:31:01 +0530

    Create derivedclass fn is overridne.cpp

---
+++ derivedclass fn is overridne.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class person{
+    public:
+    void introduce(){
+        cout<<"person is called\n";
+    }
+};
+class student : public person{
+   public: void introduce(){
+        cout<<"student is called\n";           //fn is overriden
+    }
+};
+int main() {
+   student vishal;
+   vishal.introduce();
+}
