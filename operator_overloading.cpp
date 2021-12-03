bb7f0b85abe82bfd98b982f4b410cd7130b00b16
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-12-03 15:34:24 +0530

    Create operator_overloading.cpp

---
+++ operator_overloading.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class person{
+    
+    int intmark;
+    int extmark;
+    public:
+    person(){
+        intmark=0;
+        extmark=0;
+    }
+    person(int imark,int emark){
+        intmark=imark;
+        extmark=emark;
+    }
+    void display(){
+        cout<<intmark<<endl<<extmark;
+    }
+    person operator+(person m){
+        person temp;
+        temp.intmark=intmark+m.intmark;
+        temp.extmark=extmark + m.extmark;
+        return temp;
+    }
+};
+
+int main() {
+  person m1(10,20),m2(30,20);
+  person m3=m1+m2;
+ 
+  m3.display();
+}
