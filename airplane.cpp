65489dc988ea5981cc875006e685b4b252121052
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-12-29 14:42:13 +0530

    Create airplane.cpp

---
+++ airplane.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define p1 (j/j1>j1)?p2=j+1:p2=j 
+#define l     if(n%s==0)
+    #define m    if(j1%p==0)
+#define a cin>>k>>n>>s>>p;
+using namespace std;
+class Airplanes
+{
+    public:int Packs(int k,int n,int s,int p){
+        int j;
+        l{
+            j=n/s;
+        }else
+        j=n/s+1;
+        int j1=(j)*k;
+        m return j1/p;
+         else  return j1/p+1;
+}};int main()
+{ int k,n,s,p;
+a;
+Airplanes Buying;
+int k1=Buying.Packs(k,n,s,p);
+cout<<k1;
+	return 0;}
